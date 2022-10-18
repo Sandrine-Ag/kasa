79a0055fb313e923ab73b6375dcfb263569f1236
Tchacorom Sandrine <doudouvi.sand@gmail.com>
2022-10-18 14:04:17 +0000

    step6 ok page d'erreur

--- src/pages/Erreur.js
+++ src/pages/Erreur.js
@@ -1,9 +1,14 @@
-
-
+import { Link } from "react-router-dom";
 
 
 function Erreur () {
-
+    return (
+        <div className="Erreur">
+            <h1 className="titreErreur">404</h1>
+            <p className="descErreur">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="linkErreur" to = "*">Retourner sur la page d'accueil</Link> 
+        </div>
+    )
 }
 
 export default Erreur;
